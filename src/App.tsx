ba0e1963b01eba841ea9c6bbad530134a6cae825
Przemysław Trzepiński <ptrzepinski@gmail.com>
2020-06-19 22:43:10 +0200

    added loader ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import apiData from "./api";
 import PersonInfo from "./PersonInfo";
+import Loader from "./components/Loader/Loader";
 
 function App() {
   const [data, setData] = React.useState([]);
   const [selected, setSelected] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(false);
 
   //  TODO fetch contacts using apiData function, handle loading and error states
 
@@ -13,12 +15,15 @@ function App() {
       <aside className="selected">Selected contacts: {selected.length}</aside>
 
       <section>
-        <ul className="list">
-          {data.map(personInfo => (
-            // @ts-ignore
-            <PersonInfo key={personInfo.id} data={personInfo} />
-          ))}
-        </ul>
+        {isLoading && <Loader />}
+        {!isLoading && (
+          <ul className="list">
+            {data.map(personInfo => (
+              // @ts-ignore
+              <PersonInfo key={personInfo.id} data={personInfo} />
+            ))}
+          </ul>
+        )}
       </section>
     </main>
   );
